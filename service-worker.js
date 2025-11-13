ebc3e2b85e32a6aefeed2907672e09ea998dd6fb
rafaelpinheirolacerda <rafaelpinheirolacerda@gmail.com>
2025-11-13 15:43:25 -0300

    Add service worker for caching HTML and manifest
    Implement a service worker for caching resources.

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "desafio-agua-v1";
+const URLS_TO_CACHE = [
+  "/teste.html",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
